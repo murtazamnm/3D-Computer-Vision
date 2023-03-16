427005b3dc2c7497ae25e747e810e1ad78f4d457
Murtaza <42022929+murtazamnm@users.noreply.github.com>
2023-03-16 22:26:37 +0100

    Update c.sh

--- Inverse-Perspective-Mapping/c.sh
+++ Inverse-Perspective-Mapping/c.sh
@@ -1,3 +1,3 @@
 g++ -c MatrixReaderWriter.cpp 
-g++ -c -I/usr/include/opencv4/ Panorama.cpp
-g++ -I/usr/include/opencv4/ MatrixReaderWriter.o Panorama.o -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -o panorama
+g++ -c -I/usr/include/opencv4/ Pers.cpp
+g++ -I/usr/include/opencv4/ MatrixReaderWriter.o Pers.o -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs -o Pers
